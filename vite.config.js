a7f190128cd7bd1bbb32c91b58480d9ef7ee45a1
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-06-25 20:45:12 -0300

    implementado https

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import { manifestForPlugIn } from './manifest';
-
+import { manifestForPlugIn } from "./manifest";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugIn)],
-})
-
+  server: { https: true },
+  plugins: [react(), VitePWA(manifestForPlugIn), mkcert()],
+});

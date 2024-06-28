dad94a96063ab93090aa67f2cbb864c7cb32f53e
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-06-28 15:37:53 -0300

    button logout (so leva para tela de login)

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
+import { Link } from "react-router-dom";
 
 const Settings = () => {
   return (
     <div>
+      <Link to={"/"}>
+        <h1>Logout</h1>
+      </Link>
       <Footer />
     </div>
   );

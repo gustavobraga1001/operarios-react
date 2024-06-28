6ad876fcc564d6803fc6178a857732a3d2fe2b33
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-06-28 15:45:31 -0300

    fix

--- manifest.js
+++ manifest.js
@@ -1,42 +1,41 @@
 // you can copy the base structure of manifest object.
 export const manifestForPlugIn = {
-  registerType: "prompt",
-  includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
-  manifest: {
-    name: "Operários",
-    short_name: "Operários",
-    description: "I am a simple vite app",
-    icons: [
-      {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "favicon",
+    registerType:'prompt',
+    includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+    manifest:{
+      name:"Operários",
+      short_name:"Operários",
+      description:"I am a simple vite app",
+      icons:[{
+        src: '/android-chrome-192x192.png',
+        sizes:'192x192',
+        type:'image/png',
+        purpose:'favicon'
       },
       {
-        src: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "favicon",
+        src:'/android-chrome-512x512.png',
+        sizes:'512x512',
+        type:'image/png',
+        purpose:'favicon'
       },
       {
-        src: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-        purpose: "apple touch icon",
+        src: '/apple-touch-icon.png',
+        sizes:'180x180',
+        type:'image/png',
+        purpose:'apple touch icon',
       },
       {
-        src: "/maskable_icon.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable",
-      },
+        src: '/maskable_icon.png',
+        sizes:'512x512',
+        type:'image/png',
+        purpose:'any maskable',
+      }
     ],
-    theme_color: "#000000",
-    background_color: "#000000",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    orientation: "portrait",
-  },
-};
+    theme_color:'#171717',
+    background_color:'#f0e7db',
+    display:"standalone",
+    scope:'/',
+    start_url:"/",
+    orientation:'portrait'
+    }
+  }

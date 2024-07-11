a769ec15a607e5d49a82274781ae2f4666533db6
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-07-11 11:42:24 -0300

    Tela de lista de escala atualizada

--- src/pages/ListScale/ListScale.jsx
+++ src/pages/ListScale/ListScale.jsx
@@ -2,8 +2,43 @@ import { CaretLeft } from "@phosphor-icons/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import "./ListScale.css";
+import useEvents from "../../context/EventsProvider/useEvents";
+import { useQuery } from "react-query";
+import LoadingSpinner from "../../components/Loading/Loading";
 
 const ListScale = () => {
+  const events = useEvents();
+  const { data: eventsLeader, isLoading: isLoadingLeader } = useQuery(
+    ["events"],
+    () => events.getEventsLeader(),
+    {
+      staleTime: 100000,
+    }
+  );
+
+  if (!eventsLeader || isLoadingLeader) {
+    return <LoadingSpinner />;
+  }
+
+  const formattedDateTime = (dateTimeString) => {
+    const date = new Date(dateTimeString);
+
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Mês começa em 0
+    const weekdays = [
+      "Domingo",
+      "Segunda",
+      "Terça",
+      "Quarta",
+      "Quinta",
+      "Sexta",
+      "Sábado",
+    ];
+    const weekday = weekdays[date.getDay()];
+
+    return `${day}/${month} ${weekday}`;
+  };
+
   return (
     <div>
       <header className="header-bottom-arrow">
@@ -12,41 +47,21 @@ const ListScale = () => {
         </Link>
       </header>
       <div className="list-scales">
-        <div className="card-list-scales">
-          <p className="card-list-data">20/01 Sábado</p>
-          <div className="card-list-infos-scales">
-            <p>20:00</p>
-            <span>Gustavo Braga, Gustavo Almeida.</span>
-          </div>
-        </div>
-        <div className="card-list-scales">
-          <p className="card-list-data">20/01 Sábado</p>
-          <div className="card-list-infos-scales">
-            <p>20:00</p>
-            <span>Gustavo Braga, Gustavo Almeida.</span>
-          </div>
-        </div>
-        <div className="card-list-scales">
-          <p className="card-list-data">20/01 Sábado</p>
-          <div className="card-list-infos-scales">
-            <p>20:00</p>
-            <span>Gustavo Braga, Gustavo Almeida.</span>
-          </div>
-        </div>
-        <div className="card-list-scales">
-          <p className="card-list-data">20/01 Sábado</p>
-          <div className="card-list-infos-scales">
-            <p>20:00</p>
-            <span>Gustavo Braga, Gustavo Almeida.</span>
-          </div>
-        </div>
-        <div className="card-list-scales">
-          <p className="card-list-data">20/01 Sábado</p>
-          <div className="card-list-infos-scales">
-            <p>20:00</p>
-            <span>Gustavo Braga, Gustavo Almeida.</span>
-          </div>
-        </div>
+        {eventsLeader.length > 0 ? (
+          eventsLeader.map((event) => (
+            <div key={event.id} className="card-list-scales">
+              <p className="card-list-data">
+                {formattedDateTime(event.date_time)}
+              </p>
+              <div className="card-list-infos-scales">
+                <p>20:00</p>
+                <span>Gustavo Braga, Gustavo Almeida.</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <h1>Sem eventos</h1>
+        )}
       </div>
     </div>
   );

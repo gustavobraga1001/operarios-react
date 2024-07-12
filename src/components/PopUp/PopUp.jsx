0132e4c2103164fd455b4238ad48d4907ccb11db
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-07-12 15:23:58 -0300

    update popup

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -4,9 +4,11 @@ import "./PopUp.css";
 const PopUp = ({ isClicked, toggleDropdown }) => {
   return (
     <div className={`popup-enviar ${isClicked ? "active-popup" : ""}`}>
-      <h3>Parabéns!</h3>
-      <p>A escala foi lançada.</p>
-      <button onClick={toggleDropdown}>Ok</button>
+      <div className="popup-content">
+        <h3>Parabéns!</h3>
+        <p>A escala foi lançada.</p>
+        <button onClick={toggleDropdown}>Ok</button>
+      </div>
     </div>
   );
 };

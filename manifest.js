38c406a38df7f767339faa76f6fd6a90cc829965
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-07-15 11:26:12 -0300

    fix

--- manifest.js
+++ manifest.js
@@ -5,31 +5,31 @@ export const manifestForPlugIn = {
   manifest: {
     name: "Operários",
     short_name: "Operários",
-    description: "I am a simple vite app",
+    description: "Aplicação para operários e líderes da Igreja Bom Pastor",
     icons: [
       {
         src: "/android-chrome-192x192.jpg",
         sizes: "192x192",
         type: "image/png",
-        purpose: "favicon",
+        purpose: "any", // Alterado para 'any'
       },
       {
         src: "/android-chrome-512x512.jpg",
         sizes: "512x512",
         type: "image/png",
-        purpose: "favicon",
+        purpose: "any", // Alterado para 'any'
       },
       {
         src: "/apple-touch-icon.jpg",
         sizes: "192x192",
         type: "image/png",
-        purpose: "apple touch icon",
+        purpose: "any", // Alterado para 'any'
       },
       {
         src: "/maskable_icon.jpg",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any", // Alterado para 'any'
       },
     ],
     theme_color: "#000000",

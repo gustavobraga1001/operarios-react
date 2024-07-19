40c17ebcead17e19a473b40a7f7f529a21398a65
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-07-19 14:57:41 -0300

    test icon

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,6 +7,8 @@ importScripts(
   "https://www.gstatic.com/firebasejs/9.1.0/firebase-messaging-compat.js"
 );
 
+import icon from "./maskable_icon.jpg";
+
 // Inicialize o Firebase no service worker
 firebase.initializeApp({
   apiKey: "AIzaSyAQLATcqtrCoC4EVD9qtMTHL7Q1eqcjopo",
@@ -26,7 +28,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.data.title;
   const notificationOptions = {
     body: payload.data.body,
-    icon: payload.data.icon,
+    icon: icon,
     data: {
       click_action: "https://operarios-react.vercel.app/calendar", // Adicione a URL aqui
     },

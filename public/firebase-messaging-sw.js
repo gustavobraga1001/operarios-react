3bd038f36fd238dfc89c21d883c8c4a09548210e
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-07-26 15:17:06 -0300

    update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,17 +18,43 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
+// Receber mensagens em segundo plano
 messaging.onBackgroundMessage((payload) => {
   console.log("Received background message ", payload);
 
-  const notificationTitle = payload.data.title;
+  const notificationTitle = payload.data.title || "Título padrão";
   const notificationOptions = {
-    body: payload.data.body,
-    icon: payload.data.icon,
+    body: payload.data.body || "Corpo padrão",
+    icon: payload.data.icon || "/default-icon.png",
     data: {
-      click_action: "https://operarios-react.vercel.app/calendar",
+      click_action: "https://localhost:5173/calendar",
     },
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+// Lidar com cliques na notificação
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  const urlToOpen = new URL(
+    event.notification.data.click_action,
+    self.location.origin
+  ).href;
+
+  event.waitUntil(
+    clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url === urlToOpen && "focus" in client) {
+            return client.focus();
+          }
+        }
+        if (clients.openWindow) {
+          return clients.openWindow(urlToOpen);
+        }
+      })
+  );
 });
